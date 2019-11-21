ae976458661f5258476788148327782a31537194
x1kurosu <48033755+x1kurosu@users.noreply.github.com>
2019-11-21 00:32:46 -0700

    Update 2019-11-21.js

--- src/js/data/2019-11-21.js
+++ src/js/data/2019-11-21.js
@@ -63,7 +63,7 @@ dataSet[dataSetVersion].options = [
     checked: false,
     sub: [ { name: "Universal Century", tooltip: "OG timeline", key: "uc" }, { name: "Future Century", tooltip: "G Gundam", key: "fc" }, { name: "After Colony", tooltip: "Wing", key: "ac" }, { name: "After War", tooltip: "Gundam X", key: "aw" }, { name: "Cosmic Era", tooltip: "SEED", key: "ce" }, { name: "Anno Domini", tooltip: "00 series", key: "ad" }, { name: "Reguild Century", tooltip: "G-Reco", key: "reguild" }, { name: "Post Disaster", tooltip: "IBO", key: "pd" }, { name: "Build Series", key: "bs"}, { name: "SD Gundam", key: "sdg"}, { name: "G Generation Series", key: "ggen"}, }
     ]
-  },
+  }
 
 ];
 
@@ -3482,5 +3482,5 @@ dataSet[dataSetVersion].characterData = [
 	  series: ["ggen"],
 	  timeline: ["ggen"]
 	}
-  },
+  }
 ];
