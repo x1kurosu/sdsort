a3750b342adf0176bc95fbeed1cbf42cc58ef136
x1kurosu <48033755+x1kurosu@users.noreply.github.com>
2025-12-04 02:22:03 -0700

    Update data.js

--- src/js/data.js
+++ src/js/data.js
@@ -20,4 +20,4 @@ let dataSetVersion = '';
 /**
  * Image root, will be appended to the start of every image URL.
  */
-const imageRoot = 'https://i.imgur.com/';
+const imageRoot = 'https://raw.githubusercontent.com/x1kurosu/sdsort/refs/heads/master/src/assets/sprites/';
